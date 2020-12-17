f1a077e0d08ba05aa9fdd9c9ac7beedb16e72256
Andrey Korzhun <steelcon@bk.ru>
2020-12-17 06:05:29 +0300

    Lesson 6 - Instagram

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -12,3 +12,18 @@ class GbParseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class InstagramItem(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    image = scrapy.Field()
+
+
+class InstagramPost(InstagramItem):
+    pass
+
+
+class InstagramTag(InstagramItem):
+    pass
